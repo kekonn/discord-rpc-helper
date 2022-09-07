f435fbfafe6892b7e6ce6bb312df0d904251b49b
kekkon <149670+kekonn@users.noreply.github.com>
2022-09-07 14:24:08 +0000

    WIP push

--- src/steam/cache.rs
+++ src/steam/cache.rs
@@ -1,15 +1,89 @@
 
-use std::{path::{PathBuf}};
+use std::{path::{PathBuf, Path}};
 use anyhow::{Result, anyhow};
+use reqwest::Url;
+use scraper::{Selector, ElementRef, Html};
 
 // XDG RUNTIME HOME
 
 const XDG_RUNTIME_ENV_VAR: &str = "XDG_RUNTIME_DIR";
 const CACHE_DIR: &str = "cache";
 
+const STEAM_NAME_SELECTOR: &str = "#appHubAppName";
+const STEAM_ICON_SELECTOR: &str = "div.apphub_AppIcon img";
+
 #[derive(Debug, PartialEq)]
 pub struct DocumentCache {
+    /// The location of the document cache in the file system
     location: String,
+}
+
+impl DocumentCache {
+    async fn get_name(&self, steam_url: &str) -> Result<String> {
+        let name_selector = Selector::parse(STEAM_NAME_SELECTOR).unwrap();
+        let html = match self.download_steamdb_page(&steam_url).await {
+            Ok(h) => get_html(&h),
+            Err(err) => return Err(anyhow!(err)),
+        };
+    
+        let found_elements: Vec<ElementRef> = html.select(&name_selector).collect();
+        match found_elements.len() {
+            0 => Err(anyhow!("Could not find any name elements on page")),
+            1 => Ok(found_elements[0].inner_html()),
+            _ => Err(anyhow!("Found more than one name element on the page")),
+        }
+    }
+    
+    async fn get_appicon(&self, steam_url: &str) -> Result<String> {
+        let img_selector = Selector::parse(STEAM_ICON_SELECTOR).unwrap();
+        let html = match self.download_steamdb_page(&steam_url).await {
+            Ok(h) => get_html(&h),
+            Err(e) => return Err(anyhow!(e)),
+        };
+    
+        let found_elements: Vec<ElementRef> = html.select(&img_selector).collect();
+        match found_elements.len() {
+            0 => Err(anyhow!("Could not find the icon image on the page")),
+            1 => Ok(found_elements[0].value().attr("src").unwrap().to_string()),
+            _ => Err(anyhow!("Found more than one app icon on the page")),
+        }
+    }
+    
+    /// Downloads the given url or provides it from cache, if available
+    async fn download_steamdb_page(&self, url: &str) -> anyhow::Result<String> {
+        let document = match reqwest::get(url).await {
+            Ok(r) => r.text().await.unwrap(),
+            Err(err) => return Err(anyhow!(err)),
+        };
+    
+        Ok(document)
+    }
+
+    fn get_cache_path(&self, url: &str) -> Result<PathBuf> {
+        let steamid_url_part = Url::parse(url)?.path_segments()
+                        .ok_or_else(err)
+                        .into_iter().last().ok_or(anyhow!("Could not find path segments in url \"{}\"", url))?;
+
+        let path_buff = self.get_location_pathbuf();
+
+        path_buff.push(format!("{}.html", steamid_url_part.));
+
+        path_buff
+    }
+
+    /// Create `PathBuf` from `DocumentCache.location`
+    fn get_location_pathbuf(&self) -> PathBuf {
+        let path_buff = PathBuf::new();
+
+        path_buff.push(&self.location);
+
+        path_buff
+    }
+}
+    
+/// Parsing a string into an HTML document
+fn get_html(html: &str) -> Html {
+    Html::parse_document(html)
 }
 
 /// Builds a document cache.
@@ -91,7 +165,6 @@ fn get_runtime_path() -> Result<String> {
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
 
     use super::*;
 
@@ -102,75 +175,31 @@ mod tests {
 
         assert!(&result.is_ok(), "Found error instead: {}", result.err().unwrap());
     }
-
-    #[test]
-    fn create_cache_dir_in_root_fails() {
-        let result = create_cache_dir("/");
-
-        assert!(result.is_err());
-    }
-
-    #[test]
-    fn create_cache_dir_relative_creates_dirs() {
-
-        let result = create_cache_dir("./");
-
-        assert!(result.is_ok(), "{}", result.err().unwrap());
-        
-        // cleanup
-        cleanup_directories().unwrap();
-    }
-
-    #[test]
-    fn is_valid_location_relative_creates_cache_only() {
-
-        std::fs::create_dir_all("./discord-rpc-helper").unwrap();
-
-        let result = create_cache_dir("./discord-rpc-helper");
-
-        assert!(result.is_ok(), "{}", result.err().unwrap());
-        
-        let created_dir = result.unwrap();
-        assert!(created_dir.ends_with("cache"), "Created directory actually is {}", created_dir);
-
-        // cleanup
-        cleanup_directories().unwrap();
-    }
     
     #[test]
-    fn builds_with_default_location() {
+    #[ignore = "Running these automatically, they interfere because of the directories"]
+    fn builds_with_default_location() -> Result<()> {
         let builder = DocumentCacheBuilder::new();
-        let runtime_path = get_runtime_path().unwrap();
+        let runtime_path = get_runtime_path()?;
 
         let result = builder.build();
 
         assert!(result.is_ok(), "Failed to build builder: {}", result.err().unwrap());
-        assert!(result.unwrap().location == runtime_path);
+        assert!(result?.location == runtime_path);
+
+        Ok(())
     }
 
     #[test]
-    fn builds_with_specced_location() {
+    #[ignore = "Running these automatically, they interfere because of the directories"]
+    fn builds_with_specced_location() -> Result<()> {
         let builder = DocumentCacheBuilder::new().with_location("./");
 
         let result = builder.build();
 
         assert!(result.is_ok(), "Failed to build builder: {}", result.err().unwrap());
-        assert_eq!(result.unwrap().location, "./discord-rpc-helper/cache");
+        assert_eq!(result?.location, "./discord-rpc-helper/cache");
 
-        cleanup_directories().unwrap();
-    }
-
-    /// Cleans locally created directories
-    fn cleanup_directories() -> Result<()> {
-        let path = Path::new("./discord-rpc-helper");
-        
-        if path.exists() {
-            match std::fs::remove_dir_all(path) {
-                Ok(_) => Ok(()),
-                Err(e) => Err(anyhow!(e))
-            }
-        } else {
-            Ok(())
-        }
+        Ok(())
     }
 }
